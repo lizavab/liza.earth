360cf9e426e3b15bddf68b65e39b1bf5b4dd47e5
ndri <5224431+ndri@users.noreply.github.com>
2025-10-17 14:17:37 +0200

    Use prose-sm

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -48,7 +48,7 @@
 		class={[
 			'h-full w-full max-w-2xl border border-main-200 bg-white px-6 py-8 text-main-700',
 			'sm:h-auto sm:rounded-xl sm:px-8',
-			'prose prose-theme dark:prose-invert prose-headings:font-medium',
+			'prose prose-sm prose-theme dark:prose-invert prose-headings:font-medium',
 			'dark:border-main-700 dark:bg-main-950 dark:text-main-200'
 		]}
 	>
